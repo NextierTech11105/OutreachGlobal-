4df2b138c177ce2476409abc9a18c7184e08d168
NextierTech11105 <tb@outreachglobal.io>
2025-11-26 21:14:42 -0500

    Add default admin bootstrap for automatic admin user creation

--- apps/api/src/app/user/user.module.ts
+++ apps/api/src/app/user/user.module.ts
@@ -4,12 +4,13 @@ import { CreateUserQuestion } from "./runners/create-user.question";
 import { UserResolver } from "./resolvers/user.resolver";
 import { UserService } from "./services/user.service";
 import { ConfigModule } from "@nestjs/config";
+import { DefaultAdminBootstrap } from "./bootstrap/default-admin.bootstrap";
 
 @CustomModule({
   imports: [ConfigModule],
   runners: [UserRunner, CreateUserQuestion],
   resolvers: [UserResolver],
-  providers: [UserService],
+  providers: [UserService, DefaultAdminBootstrap],
   exports: [UserService],
 })
 export class UserModule {}
